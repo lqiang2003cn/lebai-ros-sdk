dc5190dfbdde6a90c4a23dec8126fc536763fd7b
lqiang2003cn <randycoq@163.com>
2023-11-01 11:28:29 +0800

    c

--- lebai_tutorials/scripts/ros_bridge_services.py
+++ lebai_tutorials/scripts/ros_bridge_services.py
@@ -14,35 +14,44 @@ class RosBridgeServices:
 
         # init fields
         self.aruco_list = None
-        self.aruco_mapping = {
-            '200': '咖啡',
-            '204': '矿泉水',
-            '206': '牛奶'
-        }
 
         # subscribes
         rospy.Subscriber("/aruco_marker_publisher/markers", MarkerArray, self.aruco_call_back)
 
         # creating services
         rospy.Service('get_goods_info', JsonInfo, self.get_goods_info)
+        rospy.Service('move_item_to_output_area', JsonInfo, self.move_item_to_output_area)
+
         print 'services are initialized'
 
     def aruco_call_back(self, marker_array):
         self.aruco_list = marker_array.markers
 
-    def get_goods_info(self, empty):
-        assert empty is not None
+    def get_goods_info(self, params):
+        assert params is not None
         try:
             while self.aruco_list is None:
                 rospy.sleep(0.5)  # waiting for aruco detector
 
             goods_info = []
             for al in self.aruco_list:
-                goods_info.append({
-                    'good_name': self.aruco_mapping[str(al.id)],
-                    'good_no': 1,
-                })
+                goods_info.append({'good_id': al.id, 'good_no': 1})
             return json.dumps(goods_info)
+        except rospy.ServiceException, e:
+            print "Service call failed: %s" % e
+
+    def move_item_to_output_area(self, params):
+        assert params is not None
+        try:
+            json_param = json.loads(json.loads(params.input_json_str))
+            item_id = json_param['item_id']
+
+            # move to item's top position
+
+
+            print 'item id is:' + str(json_param['item_id'])
+            res = {'status': 'ok'}
+            return json.dumps(res)
         except rospy.ServiceException, e:
             print "Service call failed: %s" % e
 
