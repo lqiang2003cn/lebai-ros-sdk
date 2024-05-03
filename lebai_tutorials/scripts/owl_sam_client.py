cc1dc8d84870bcfd4d48c3d7a687165574def0e2
lqiang2003cn <randycoq@163.com>
2024-05-03 13:00:31 +0800

    p

--- lebai_tutorials/scripts/owl_sam_client.py
+++ lebai_tutorials/scripts/owl_sam_client.py
@@ -15,26 +15,9 @@ from cv_bridge import CvBridge
 from geometry_msgs.msg import Point32
 from sensor_msgs.msg import Image
 from sensor_msgs.msg import PointCloud2, PointField, PointCloud
-import ros_numpy
+import ros_numpy as rnp
 
 import utils
-
-# The data structure of each point in ros PointCloud2: 16 bits = x + y + z + rgb
-FIELDS_XYZ = [
-    PointField(name='x', offset=0, datatype=PointField.FLOAT32, count=1),
-    PointField(name='y', offset=4, datatype=PointField.FLOAT32, count=1),
-    PointField(name='z', offset=8, datatype=PointField.FLOAT32, count=1),
-]
-FIELDS_XYZRGB = FIELDS_XYZ + [PointField(name='rgb', offset=12, datatype=PointField.UINT32, count=1)]
-
-# Bit operations
-BIT_MOVE_16 = 2 ** 16
-BIT_MOVE_8 = 2 ** 8
-convert_rgbUint32_to_tuple = lambda rgb_uint32: (
-    (rgb_uint32 & 0x00ff0000) >> 16, (rgb_uint32 & 0x0000ff00) >> 8, (rgb_uint32 & 0x000000ff))
-convert_rgbFloat_to_tuple = lambda rgb_float: convert_rgbUint32_to_tuple(
-    int(cast(pointer(c_float(rgb_float)), POINTER(c_uint32)).contents.value)
-)
 
 
 def default(obj):
@@ -50,89 +33,85 @@ class Nodo(object):
 
     def __init__(self):
         # Params
-        self.image = None
-        self.pointcloud2 = None
+        self.cam01_rgb = None
+        self.cam02_rgb = None
+        self.cam01_pc = None
+        self.cam02_pc = None
         self.depth = None
         self.texts = None
         self.br = CvBridge()
-        # Node cycle rate (in Hz).
         self.sleep_rate = rospy.Rate(1.0)
 
-        # call services
-        # self.point_cloud_service = rospy.ServiceProxy('/camera/save_point_cloud', EmptyRequest)
-
-        # Publishers
-        self.pub = rospy.Publisher('imagetimer', Image, queue_size=10)
-
         # Subscribers
-        rospy.Subscriber("/ob_camera_01/color/image_raw", Image, self.rgb_callback)
-        rospy.Subscriber("/ob_camera_01/depth_registered/points", PointCloud2, self.pointcloud2_callback)
+        rospy.Subscriber("/ob_camera_01/color/image_raw", Image, self.cam01_rgb_callback)
+        rospy.Subscriber("/ob_camera_02/color/image_raw", Image, self.cam02_rgb_callback)
+        rospy.Subscriber("/ob_camera_01/depth_registered/points", PointCloud2, self.cam01_pc_callback)
+        rospy.Subscriber("/ob_camera_02/depth_registered/points", PointCloud2, self.cam02_pc_callback)
 
-    @staticmethod
-    def convertCloudFromRosToOpen3d(ros_cloud):
+    def cam01_rgb_callback(self, msg):
+        self.cam01_rgb = self.br.imgmsg_to_cv2(msg)
 
-        parray = ros_numpy.point_cloud2.pointcloud2_to_array(ros_cloud)
+    def cam02_rgb_callback(self, msg):
+        self.cam02_rgb = self.br.imgmsg_to_cv2(msg)
 
+    def cam01_pc_callback(self, msg):
+        self.cam01_pc = msg
 
-        # mask = np.load('masks.npy')[0].reshape(-1)
-
-        # Get cloud data from ros_cloud
-        field_names = [field.name for field in ros_cloud.fields]
-        cloud_data = list(pc2.read_points(ros_cloud, skip_nans=True, field_names=field_names))
-
-        # Check empty
-        # open3d_cloud = open3d.PointCloud()
-        if len(cloud_data) == 0:
-            print("Converting an empty cloud")
-            return None
-
-        rgb = None
-
-        # Set open3d_cloud
-        if "rgb" in field_names:
-            IDX_RGB_IN_FIELD = 3  # x, y, z, rgb
-
-            # Get xyz
-            xyz = [Point32(x, y, z) for x, y, z, rgb in cloud_data]  # (why cannot put this line below rgb?)
-
-            # Get rgb
-            # Check whether int or float
-            if type(cloud_data[0][IDX_RGB_IN_FIELD]) == float:  # if float (from pcl::toROSMsg)
-                rgb = [convert_rgbFloat_to_tuple(rgb) for x, y, z, rgb in cloud_data]
-            else:
-                rgb = [convert_rgbUint32_to_tuple(rgb) for x, y, z, rgb in cloud_data]
-
-            # combine
-            # open3d_cloud.points = open3d.Vector3dVector(np.array(xyz))
-            # open3d_cloud.colors = open3d.Vector3dVector(np.array(rgb) / 255.0)
-        else:
-            xyz = [(x, y, z) for x, y, z in cloud_data]  # get xyz
-            # open3d_cloud.points = open3d.Vector3dVector(np.array(xyz))
-
-        # return
-        return rgb, xyz
-
-    def rgb_callback(self, msg):
-        self.image = self.br.imgmsg_to_cv2(msg)
-
-    def pointcloud2_callback(self, msg):
-        self.pointcloud2 = msg
+    def cam02_pc_callback(self, msg):
+        self.cam02_pc = msg
 
     def request_owl_vit(self):
-        while self.image is None:
+        while self.cam01_rgb is None:
             print "waiting for image"
             self.sleep_rate.sleep()
 
-        while self.pointcloud2 is None:
+        while self.cam02_rgb is None:
+            print "waiting for image"
+            self.sleep_rate.sleep()
+
+        while self.cam01_pc is None:
             print "waiting for pointcloud"
             self.sleep_rate.sleep()
 
-        json_data = {
-            "image": self.image.tolist(),
+        while self.cam02_pc is None:
+            print "waiting for pointcloud"
+            self.sleep_rate.sleep()
+
+        # get cam01 mask
+        cam1_json_data = {
+            "image": self.cam01_rgb.tolist(),
             "texts": self.texts
         }
+        response1 = utils.post_json_no_proxy("owl_vit", cam1_json_data)
+        cam01_mask = np.array(response1.json()['mask'])[0]
 
-        # rgb, xyz = Nodo.convertCloudFromRosToOpen3d(self.pointcloud2)
+        # get cam02 mask
+        cam2_json_data = {
+            "image": self.cam02_rgb.tolist(),
+            "texts": self.texts
+        }
+        response2 = utils.post_json_no_proxy("owl_vit", cam2_json_data)
+        cam02_mask = np.array(response2.json()['mask'])[0]
+
+        # cam01 masked pc
+        cam01_pc_array = rnp.point_cloud2.pointcloud2_to_array(self.cam01_pc).reshape((480, 640))[cam01_mask]
+        cam01_pc_xyz = rnp.point_cloud2.get_xyz_points(cam01_pc_array)
+        cam01_pc_rgb_split = rnp.point_cloud2.split_rgb_field(cam01_pc_array)
+        cam01_pc_rgb = np.zeros(cam01_pc_rgb_split.shape + (3,), dtype=np.int)
+        cam01_pc_rgb[..., 0] = cam01_pc_rgb_split['r']
+        cam01_pc_rgb[..., 1] = cam01_pc_rgb_split['g']
+        cam01_pc_rgb[..., 2] = cam01_pc_rgb_split['b']
+
+        # cam02 masked pc
+        cam02_pc_array = rnp.point_cloud2.pointcloud2_to_array(self.cam02_pc).reshape((480, 640))[cam02_mask]
+        cam02_pc_xyz = rnp.point_cloud2.get_xyz_points(cam02_pc_array)
+        cam02_pc_rgb_split = rnp.point_cloud2.split_rgb_field(cam02_pc_array)
+        cam02_pc_rgb = np.zeros(cam02_pc_rgb_split.shape + (3,), dtype=np.int)
+        cam02_pc_rgb[..., 0] = cam02_pc_rgb_split['r']
+        cam02_pc_rgb[..., 1] = cam02_pc_rgb_split['g']
+        cam02_pc_rgb[..., 2] = cam02_pc_rgb_split['b']
+
+        # merge cam01 and cam02
 
         p = PointCloud()
         # p.header.stamp = rospy.Time.now()
@@ -149,19 +128,24 @@ class Nodo(object):
         while not rospy.is_shutdown():
             try:
                 listener.waitForTransform("base_link", "ob_camera_01_color_optical_frame", rospy.Time(), rospy.Duration(4))
-            # r = sensor_msgs.msg.PointCloud2()
-            # r.header.stamp = self.pointcloud2.header.stamp
-            # r.header.frame_id = self.pointcloud2.header.frame_id
-            # self.pointcloud2.header.frame_id = "/" + self.pointcloud2.header.frame_id
-            # mat44 = listener.asMatrix("/base_link", self.pointcloud2.header)
-            # def xf(p):
-            #     xyz = tuple(numpy.dot(mat44, numpy.array([p.x, p.y, p.z, 1.0])))[:3]
-            #     return geometry_msgs.msg.Point(*xyz)
-            # r.points = [xf(p) for p in self.pointcloud2.points]
+                # translation,rotation = listener.lookupTransform(target_frame, hdr.frame_id, hdr.stamp)
+                # fromTranslationRotation(translation, rotation)
+                #
+                #
+                # listener.
+                # r = sensor_msgs.msg.PointCloud2()
+                # r.header.stamp = self.pointcloud2.header.stamp
+                # r.header.frame_id = self.pointcloud2.header.frame_id
+                # self.pointcloud2.header.frame_id = "/" + self.pointcloud2.header.frame_id
+                # mat44 = listener.asMatrix("/base_link", self.pointcloud2.header)
+                # def xf(p):
+                #     xyz = tuple(numpy.dot(mat44, numpy.array([p.x, p.y, p.z, 1.0])))[:3]
+                #     return geometry_msgs.msg.Point(*xyz)
+                # r.points = [xf(p) for p in self.pointcloud2.points]
 
-                b = listener.asMatrix(p.header.frame_id, b.header)
-                np.save("matrix_cam_to_base.npy", b)
-                p = listener.transformPointCloud("base_link", p)
+                # b = listener.asMatrix(p.header.frame_id, b.header)
+                # np.save("matrix_cam_to_base.npy", b)
+                # p = listener.transformPointCloud("base_link", p)
                 break
             except Exception as e:
                 self.sleep_rate.sleep()
@@ -173,15 +157,11 @@ class Nodo(object):
         #     )
         #     cloud_out = do_transform_cloud(self.pointcloud2, trans)
 
-
-
-
-        response = utils.post_json_no_proxy("owl_vit", json_data)
-        try:
-            # masks_from_server = np.array(, dtype=np.bool)
-            print("server response:" + response.json()["response"])
-        except requests.exceptions.RequestException:
-            print(response.text)
+        # try:
+        #     # masks_from_server = np.array(, dtype=np.bool)
+        #     print("server response:" + response.json()["response"])
+        # except requests.exceptions.RequestException:
+        #     print(response.text)
 
     def start(self):
         rospy.loginfo("Timing images")
@@ -189,8 +169,8 @@ class Nodo(object):
         while not rospy.is_shutdown():
             rospy.loginfo('publishing image')
             # br = CvBridge()
-            if self.image is not None:
-                self.pub.publish(self.br.cv2_to_imgmsg(self.image))
+            if self.cam01_rgb is not None:
+                self.pub.publish(self.br.cv2_to_imgmsg(self.cam01_rgb))
             self.sleep_rate.sleep()
 
 
