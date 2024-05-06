293683f13ea5ad004f38d619fcf097279e3870ec
lqiang2003cn <randycoq@163.com>
2024-05-06 23:05:36 +0800

    p

--- lebai_tutorials/scripts/adjust_camera.py
+++ lebai_tutorials/scripts/adjust_camera.py
@@ -37,18 +37,50 @@ def markers_callback(data):
 
 
 if __name__ == "__main__":
+    rospy.init_node('get_camera_frame')
+    listener = tf.TransformListener()
+
     # step1: run roslaunch (you might need to unplug one camera when you have multiple cameras)
     # roslaunch lebai_lm3_moveit_config gemini2_adjust_camera.launch
 
-    # step2: activate the aruco topic: run the command in a terminal continuously
-    # rostopic echo /aruco_single/marker
-
-    # step3: run this node
+    # step2: adjust camera 01:
+    # step2.1: put the 5cm aruco box to a position
+    # step2.2: adjust num_of_x_incr_blocks, num_of_y_incr_blocks based on the position of the 5cm box
+    marker_in_optical = autils.query_pose(listener, 'ob_camera_01_color_optical_frame', 'cam01_aruco_marker_frame')
+    color_optical_to_aruco_pos = marker_in_optical[0]
+    color_optical_to_aruco_ori = marker_in_optical[1]
+    marker_in_optical = autils.get_matrix_from_pos_and_quat(color_optical_to_aruco_pos, color_optical_to_aruco_ori)
+    box_in_world = np.eye(4)
+    box_in_world[0:3, 0] = [-1, 0, 0]
+    box_in_world[0:3, 1] = [0, -1, 0]
+    box_in_world[0:3, 2] = [0, 0, 1]
+    position1_x = 0
+    position1_y = -0.2348
+    num_of_x_incr_blocks = 1
+    num_of_y_incr_blocks = 0
+    len_of_x = 0.068  # JBL
+    len_of_y = 0.082  # JBL
+    x_to_aruco_center = 0.03  # 5cm box
+    y_to_aruco_center = 0.03  # 5cm box
+    x_center = position1_x + len_of_x * num_of_x_incr_blocks + x_to_aruco_center
+    y_center = position1_y - len_of_y * num_of_y_incr_blocks - y_to_aruco_center
+    z_center = 0.105
+    box_in_world[0:3, 3] = [x_center, y_center, z_center]
+    color_optical_in_world = np.matmul(box_in_world, np.linalg.inv(marker_in_optical))
+    # optical_to_camera_pos = autils.query_pose(listener, 'camera_color_optical_frame', 'camera_link')
+    optical_to_camera_pos = [-0.014, 0.000, -0.002]
+    optical_to_camera_quat = [0.501, -0.500, 0.498, 0.501]
+    optical_to_camera_matrix = autils.get_matrix_from_pos_and_quat(optical_to_camera_pos, optical_to_camera_quat)
+    camera_in_world = np.matmul(color_optical_in_world, optical_to_camera_matrix)
+    print np.concatenate([translation_from_matrix(camera_in_world), quaternion_from_matrix(camera_in_world)])
+    # step2.3: run the command in a terminal continuously to activate the aruco topic:
+    # rostopic echo /cam01_aruco_single/marker
+    # step2.4: run this node
     # get result, for example: 0.50033641 -0.63821168  0.38514528 -0.14800998  0.01318743  0.97093654 0.18762029
 
     # step4: put the result in the static_transform_publisher node in the marker_publisher.launch file
-    rospy.init_node('get_camera_frame')
-    listener = tf.TransformListener()
+
+
     marker_in_optical = autils.query_pose(listener, 'camera_color_optical_frame', 'aruco_marker_frame')
     color_optical_to_aruco_pos = marker_in_optical[0]
     color_optical_to_aruco_ori = marker_in_optical[1]
@@ -71,6 +103,12 @@ if __name__ == "__main__":
     y_center = position1_y - len_of_y * num_of_y_incr_blocks - y_to_aruco_center
     z_center = 0.105
     box_in_world[0:3, 3] = [x_center, y_center, z_center]
+
+    # camera 1 aruco
+    # x: -0.0182075120141
+    # y: -0.397250032065
+    # z: 0.019383107582
+
 
     color_optical_in_world = np.matmul(box_in_world, np.linalg.inv(marker_in_optical))
     # optical_to_camera_pos = autils.query_pose(listener, 'camera_color_optical_frame', 'camera_link')
---
+++ lebai_tutorials/scripts/adjust_multiple_camera.py
@@ -0,0 +1,95 @@
+import numpy as np
+import rospy
+import tf
+from tf.transformations import quaternion_from_matrix, quaternion_matrix, euler_from_quaternion, translation_from_matrix
+import arm_utils as autils
+import aruco_msgs.msg as am
+
+
+def adjust_camera_01():
+    rospy.init_node('get_camera_frame')
+    listener = tf.TransformListener()
+
+    # step1: run roslaunch (you might need to unplug one camera when you have multiple cameras)
+    # roslaunch lebai_lm3_moveit_config gemini2_adjust_camera.launch
+
+    # step2: adjust camera 01:
+    # step2.1: put the 5cm aruco box to a position,
+    # step2.2: adjust num_of_x_incr_blocks, num_of_y_incr_blocks based on the position of the 5cm box
+    marker_in_optical = autils.query_pose(listener, 'ob_camera_01_color_optical_frame', 'cam01_aruco_marker_frame')
+    color_optical_to_aruco_pos = marker_in_optical[0]
+    color_optical_to_aruco_ori = marker_in_optical[1]
+    marker_in_optical = autils.get_matrix_from_pos_and_quat(color_optical_to_aruco_pos, color_optical_to_aruco_ori)
+    box_in_world = np.eye(4)
+    box_in_world[0:3, 0] = [-1, 0, 0]
+    box_in_world[0:3, 1] = [0, -1, 0]
+    box_in_world[0:3, 2] = [0, 0, 1]
+    position1_x = 0
+    position1_y = -0.2348
+    num_of_x_incr_blocks = 1  # if this is positive, put x-y corner to the right of the grid
+    num_of_y_incr_blocks = 0
+    len_of_x = 0.068  # JBL
+    len_of_y = 0.082  # JBL
+    x_to_aruco_center = 0.03 if num_of_x_incr_blocks >= 0 else -0.03
+    y_to_aruco_center = 0.03  # 5cm box
+    x_center = position1_x + len_of_x * num_of_x_incr_blocks + x_to_aruco_center
+    y_center = position1_y - len_of_y * num_of_y_incr_blocks - y_to_aruco_center
+    z_center = 0.105
+    box_in_world[0:3, 3] = [x_center, y_center, z_center]
+    color_optical_in_world = np.matmul(box_in_world, np.linalg.inv(marker_in_optical))
+    optical_to_camera_pos = [-0.014, 0.000, -0.002]
+    optical_to_camera_quat = [0.501, -0.500, 0.498, 0.501]
+    optical_to_camera_matrix = autils.get_matrix_from_pos_and_quat(optical_to_camera_pos, optical_to_camera_quat)
+    camera_in_world = np.matmul(color_optical_in_world, optical_to_camera_matrix)
+    print np.concatenate([translation_from_matrix(camera_in_world), quaternion_from_matrix(camera_in_world)])
+    # step2.3: run the command in a terminal continuously to activate the aruco topic:
+    # rostopic echo /cam01_aruco_single/marker
+    # step2.4: run this node
+    # get result, for example: 0.50777705 -0.29590085  0.38367296 -0.21302105 -0.03441092  0.9723995 -0.08875327
+
+
+def adjust_camera_02():
+    rospy.init_node('get_camera_frame')
+    listener = tf.TransformListener()
+
+    # step1: run roslaunch (you might need to unplug one camera when you have multiple cameras)
+    # roslaunch lebai_lm3_moveit_config gemini2_adjust_camera.launch
+
+    # step2: adjust camera 02:
+    # step2.1: put the 5cm aruco box to a position
+    # step2.2: adjust num_of_x_incr_blocks, num_of_y_incr_blocks based on the position of the 5cm box
+    marker_in_optical = autils.query_pose(listener, 'ob_camera_02_color_optical_frame', 'cam02_aruco_marker_frame')
+    color_optical_to_aruco_pos = marker_in_optical[0]
+    color_optical_to_aruco_ori = marker_in_optical[1]
+    marker_in_optical = autils.get_matrix_from_pos_and_quat(color_optical_to_aruco_pos, color_optical_to_aruco_ori)
+    box_in_world = np.eye(4)
+    box_in_world[0:3, 0] = [-1, 0, 0]
+    box_in_world[0:3, 1] = [0, -1, 0]
+    box_in_world[0:3, 2] = [0, 0, 1]
+    position1_x = 0
+    position1_y = -0.2348
+    num_of_x_incr_blocks = -2  # if this is negative, put the -x-y corner to the left of the grid
+    num_of_y_incr_blocks = 0
+    len_of_x = 0.068  # JBL
+    len_of_y = 0.082  # JBL
+    x_to_aruco_center = 0.03 if num_of_x_incr_blocks >= 0 else -0.03  # if num_of_x_incr_blocks is negative, this should be negative
+    y_to_aruco_center = 0.03  # 5cm box
+    x_center = position1_x + len_of_x * num_of_x_incr_blocks + x_to_aruco_center
+    y_center = position1_y - len_of_y * num_of_y_incr_blocks - y_to_aruco_center
+    z_center = 0.105
+    box_in_world[0:3, 3] = [x_center, y_center, z_center]
+    color_optical_in_world = np.matmul(box_in_world, np.linalg.inv(marker_in_optical))
+    optical_to_camera_pos = [-0.014, 0.000, -0.002]
+    optical_to_camera_quat = [0.501, -0.500, 0.498, 0.501]
+    optical_to_camera_matrix = autils.get_matrix_from_pos_and_quat(optical_to_camera_pos, optical_to_camera_quat)
+    camera_in_world = np.matmul(color_optical_in_world, optical_to_camera_matrix)
+    print np.concatenate([translation_from_matrix(camera_in_world), quaternion_from_matrix(camera_in_world)])
+    # step2.3: run the command in a terminal continuously to activate the aruco topic:
+    # rostopic echo /cam02_aruco_single/marker
+    # step2.4: run this node
+    # get result, for example: -0.5489696  -0.32607117  0.38802943 -0.0421436   0.24360084  0.08854182 0.96490564
+
+
+if __name__ == "__main__":
+    adjust_camera_01()
+    # adjust_camera_02()
